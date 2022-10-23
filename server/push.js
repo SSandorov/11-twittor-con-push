82e8ba69530d5267af4edf333b82840b8fc78409
ssandorov <49904591+SSandorov@users.noreply.github.com>
2022-10-23 14:33:05 +0200

    almacenar de manera persistente las suscripciones

--- server/push.js
+++ server/push.js
@@ -1,10 +1,13 @@
+// almacenamos información de manera persistente de la siguiente manera
+const fs = require('fs');
+
 // añadimos la encriptación
 const urlSafeBase64 = require('urlsafe-base64');
 
 // añadimos el módulo push que empleamos para pasar la llave pública al servicio GET
 const vapid = require('./vapid.json');
 
-const suscripciones = [];
+const suscripciones = require('./subs-db.json');
 
 module.exports.getKey = () => {
     return urlSafeBase64.decode(vapid.publicKey);
@@ -13,5 +16,5 @@ module.exports.getKey = () => {
 module.exports.addSubscription = (suscripcion) => {
     suscripciones.push(suscripcion);
 
-    console.log(suscripciones);
+    fs.writeFileSync(`${ __dirname }/subs-db.json`, JSON.stringify(suscripciones));
 };
